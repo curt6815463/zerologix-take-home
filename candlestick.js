d49c10d6f1939d0d373c68c2cd4f68c894f63590
curt <curt6815463@gmail.com>
2022-08-07 21:08:37 +0800

    add eslint

--- candlestick.js
+++ candlestick.js
@@ -2,17 +2,26 @@ class Candlesticks {
   constructor(options) {
     this.canvas = options.canvas;
     this.ctx = this.canvas.getContext("2d");
-    this.data = options.data;
+    this.timeSeries = options.data;
+    this.properties = Object.values(this.timeSeries)
+      .map((timeSerie) => {
+        delete timeSerie.volume;
+        return timeSerie;
+      })
+      .reverse();
   }
 
-  // arrayOfAllPrices() {
-
-  // }
+  arrayOfAllPrices(array) {
+    return this.properties.reduce((result, property) => {
+      return [...result, ...Object.values(property)];
+    }, []);
+  }
 
   draw() {
+    console.log(this.arrayOfAllPrices(this.properties));
     this.ctx.save();
     this.ctx.fillStyle = "black";
-    this.ctx.fillRect(1, 1, 400, 10);
+    this.ctx.fillRect(0, 0, 400, 10);
     this.ctx.restore();
   }
 }
